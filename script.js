c4996c29ac69c24eb74c7078b8202225d91d144a
Nina Turner <cfratto87@gmail.com>
2023-05-22 15:26:11 -0500

    Continuing to flesh out JS

--- script.js
+++ script.js
@@ -21,6 +21,27 @@ closeForm.addEventListener('click', (e) => {
     popUp.classList.remove('active');
 });
 
+let myLibrary = [
+    {
+        title: 'The Hobbit, or There and Back Again',
+        author: 'J.R.R. Tolkien',
+        pages: 310,
+        read: true,
+    },
+    {
+        title: 'The Hunger Games',
+        author: 'Suzanne Collins',
+        pages: 374,
+        read: false,
+    },
+    {
+        title: 'Harry Potter and the Order of the Phoenix',
+        author: 'J.K. Rowling',
+        pages: 870,
+        read: true,
+    },
+]
+
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -28,4 +49,69 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
+};
+
+// Adds book to library
+bookForm.addEventListener('submit', (e) => {
+    popUp.classList.remove('active');
+    e.preventDefault();
+    const book = new Book(title.value, author.value, pages.value, read.checked);
+    myLibrary.push(book);
+    pringBooks();
+});
+
+function printBooks() {
+    library.textContent = '';
+    myLibrary.forEach((book, index) => {
+        addBookToLibrary(book, index);
+    });
 }
+
+function addBookToLibrary(book, index){
+    //create book card
+    const newBookCard = document.createElement('div');
+    newBookCard.classList.add('card');
+
+    // Adds title to div
+    const cardTitle = document.createElement('p');
+    cardTitle.classList.add('book-title');
+    cardTitle.innerText = book.title;
+    newBookCard.append(cardTitle);
+
+    //adds author to div
+    const cardAuthor = document.createElement('p');
+    cardAuthor.classList.add('book-author');
+    cardAuthor.innerText = book.author;
+    newBookCard.append(cardAuthor);
+
+    //adds pages to div
+    const cardPages = document.createElement('p');
+    cardPages.classList.add('book-pages');
+    cardPages.innerText = book.pages + ' pages';
+    newBookCard.append(cardPages);
+
+    //adds read checkbox to div
+    const cardCheckBox = document.createElement('input');
+    cardCheckBox.setAttribute('data', index);
+    cardCheckBox.setAttribute('type', 'checkbox');
+    cardCheckBox.setAttribute('id', `checkbox${index}`);
+    cardCheckBox.setAttribute('name', `checkbox${index}`);
+    cardCheckBox.classList.add('card-checkbox');
+    cardCheckBox.checked = book.read;
+    newBookCard.append(cardCheckBox);
+  
+
+
+    //adds remove button to div
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('data', index);
+    removeBtn.classList.add('remove-btn');
+    removeBtn.innerText = 'Remove'
+    newBookCard.append(removeBtn);
+
+    library.append(newBookCard);
+
+
+}
+
+printBooks();
